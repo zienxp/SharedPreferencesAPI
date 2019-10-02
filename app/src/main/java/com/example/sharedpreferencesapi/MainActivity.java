be525a9cce49c15b7be19728226cf36dce9038dd
ahmed.zien <ahmed.zien@gmail.com>
2019-10-02 21:59:50 +0200

    update...

--- app/src/main/java/com/example/sharedpreferencesapi/MainActivity.java
+++ app/src/main/java/com/example/sharedpreferencesapi/MainActivity.java
@@ -3,23 +3,22 @@ package com.example.sharedpreferencesapi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    private SheredPreferenceConfig preferenceConfig;
-    private EditText UserName,UserPasword;
+    private EditText UserName, UserPassword;
+    private SharedPreferenceConfig preferenceConfig;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        preferenceConfig = new SheredPreferenceConfig(getApplicationContext());
+        preferenceConfig = new SharedPreferenceConfig(getApplicationContext());
 
         UserName = findViewById(R.id.user_name);
-        UserPasword = findViewById(R.id.user_password);
+        UserPassword = findViewById(R.id.user_password);
 
         if(preferenceConfig.readLoginStatus()){
             startActivity(new Intent(this,SuccessActivity.class));
@@ -29,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void loginUser(View view) {
         String userName = UserName.getText().toString();
-        String userPassword = UserPasword.getText().toString();
+        String userPassword = UserPassword.getText().toString();
 
 
         if (userName.equals(getResources().getString(R.string.user_name))&& userPassword.equals(getResources().getString(R.string.user_password))){
@@ -41,7 +40,7 @@ public class MainActivity extends AppCompatActivity {
         else {
             Toast.makeText(this,"Login Faild....Try Again...",Toast.LENGTH_SHORT).show();
             UserName.setText("");
-            UserPasword.setText("");
+            UserPassword.setText("");
         }
 
     }
---
+++ app/src/main/java/com/example/sharedpreferencesapi/SuccessActivity.java
@@ -0,0 +1,25 @@
+package com.example.sharedpreferencesapi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class SuccessActivity extends AppCompatActivity {
+    private SharedPreferenceConfig preferenceConfig;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_success);
+
+        preferenceConfig = new SharedPreferenceConfig(getApplicationContext());
+    }
+
+    public void userLogOut(View view) {
+        preferenceConfig.writeLoginStatus(false);
+        startActivity(new Intent(this,MainActivity.class));
+        finish();
+    }
+}
